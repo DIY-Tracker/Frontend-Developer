ff73edc129557f35bb5a594502ce6b2a73f58696
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-09-23 18:14:24 -0600

    Sign up added

--- diy-tracker/src/App.js
+++ diy-tracker/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from './components/Login';
 import { Navbar } from './components/Navbar';
+import SignUp from './components/SignUp'
 
 import UserProfile from './components/UserProfile';
 import AddProject from './components/AddProject';
@@ -12,7 +13,9 @@ function App() {
     <div className="App">
       <Navbar />
       <h1>DIY Tracker</h1>
-      <Route path="/login" component={Login}/>        
+      <Route path="/login" component={Login}/>       
+      <Route path="/signup" component={SignUp}/>   
+
       <Route path='/users/:userId' component={UserProfile} />
       <Route path='/users/:userId/add' component={AddProject} />
     </div>
