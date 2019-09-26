2b026ced69908abfee383413ce6aee41c9636d1c
Rashmi <poddar.rashmi@gmail.com>
2019-09-26 11:35:23 -0400

    Implemented add project functionality

--- diy-tracker/src/App.js
+++ diy-tracker/src/App.js
@@ -42,8 +42,7 @@ function App() {
       return <EditForm {...props} />
         }}
         />  
-      <Route path='/users/:userId' component={UserProfile} />
-      <Route path='/users/:userId/add' component={AddProject} />
+      <Route exact path='/users/:userId/add' component={AddProject} />
     </div>
     </Router>
     </ProjectContext.Provider>
